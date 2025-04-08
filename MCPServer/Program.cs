cf2d55fb4d5fa7e71d11bd91c7ee4c440cadcd0b
Ramazan Altıntop <112395498+ramazanaltintop@users.noreply.github.com>
2025-04-09 01:40:21 +0300

    Program.cs updated

--- MCPServer/Program.cs
+++ MCPServer/Program.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Logging.AddConsole(consoleLogOptions =>
-{
-    // Configure all logs to go to stderr
-    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
-});
+var builder = Host.CreateApplicationBuilder(settings: null);
+
 builder.Services
     .AddMcpServer()
     .WithStdioServerTransport()
